bb6717bf34f4301603cd6a7b316f3f73ecf07fd6
stephanielam0506 <stephanielamtw@gmail.com>
2024-03-01 15:04:35 -0800

    Added CreateEvent page and Form component for the app.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import EventPage from './EventPage';
 import Nav from './Nav';
+import Form from './CreateEvent';
 
 export default function App(props) {
   return (
@@ -10,7 +11,7 @@ export default function App(props) {
         <Nav />
         <Routes>
           <Route path="/Home" element={<Home />} />
-          {/* <Route path="/CreateEvent" element={<CreateEventComponent />} /> */}
+          <Route path="/CreateEvent" element={<Form />} />
           {/* <Route path="/MyEvent" element={<MyEventComponent />} /> */}
           {/* <Route path="/Notification" element={<NotificationComponent />} /> */}
           <Route path="/EventPage" element={<EventPage />} />
