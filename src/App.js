8c80cd7666405109949b97d6698c8b11cad4baeb
aminahmackeen <maisara@uw.edu>
2024-03-01 14:55:26 -0800

    -added MyEvents, Card components
    -added style, index.css
    
    - edited index.js and App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+//import ReactDOM from 'react-dom/client';
+import { MyEvents } from './components/MyEvents';
+
+function App(props) {
+    return (
+      <div>
+        <MyEvents />
+      </div>
+    );
+}
+
+export default App;
