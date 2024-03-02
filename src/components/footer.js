c415d9df6fe4542fd7bbec6cce950ea13c028290
oscar_shen <cshen5@uw.edu>
2024-03-01 22:32:14 -0800

    Add Ella

---
+++ src/components/footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <footer>
+            <div className='container'>
+                <p><a href="mailto:rso@.uw.edu"><span class="material-icons">email</span> rso.uw.edu</a></p>
+                <p><a href="tel:000-000-0000"><span class="material-icons">phone</span> 000-000-0000</a></p>
+                <p>&copy; University of Washington Registered Student Organizations</p>
+                 <p>Created by: Ella Weinberg, Oscar Shen, Stephanie Lam, Maisara Mackeen</p>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
