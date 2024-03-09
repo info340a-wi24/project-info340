e55ac93246ba2c827c2e83e59efc18fdf8147b51
oscar_shen <cshen5@uw.edu>
2024-03-09 14:55:10 -0800

    update event page

--- src/components/EventPage.js
+++ src/components/EventPage.js
@@ -3,20 +3,31 @@ import Nav from './Nav';
 
 
 export default function EventPage() {
+  const uData = [{
+    title: "Timeless Taiwan",
+    location: "HUB 224",
+    date: "02/10/2024",
+    description: "Attention everyone‼️ Brace yourselves for the exhilarating extravaganza of the year— TOSA’s annual event, TIMELESS TAIWAN: Journey Through Time! ✨⏳ Immerse yourself in the vibrant world of Taiwanese street food, classic night market games, and breathtaking performances🤩Stay tuned for more exciting updates...",
+    image: "img/big.png",
+    alt: "Students attending event the previous year",
+    hashtags: "#tosa #timelesstaiwan #nightmarket"}];
+
+  const { title, location, image, date, description } = uData[0];
+
   return(
     <div>
       <Nav />
-      <header style={{ backgroundImage: `./img/big.png`, height: '450px' }}>
-          <h1 className="BigEvent EventTitle">Timeless Taiwan</h1>
+      <header style={{ backgroundImage: `url(${image})`, height: '450px' }}>
+          <h1 className="BigEvent EventTitle">{title}</h1>
       </header>
 
       <main>
       <div className="event_info">
         <div className="Titles">
-          <h5><div className="event_details"><strong className="colorTitle">Time: </strong> 2024.03.09 (Sat) 19:30 - 20:20</div></h5>
-          <h5><div className="event_details"><strong className="colorTitle">Location: </strong>After purchase completed, you can enter the live stream from the ticket page.</div></h5>
+          <h5><div className="event_details"><strong className="colorTitle">Time: </strong> {date}</div></h5>
+          <h5><div className="event_details"><strong className="colorTitle">Location: </strong>{location}</div></h5>
         </div>
-        <h4><div className="event_description">Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</div></h4>
+        <h4><div className="event_description">{description}</div></h4>
         <a href="#" className="btn btn-dark register">Register</a>
       </div>
     </main>
