a872e1f4e083264640a9d72c278dd539a8da1f08
stephanielam0506 <stephanielamtw@gmail.com>
2024-03-11 14:59:38 -0700

    Updated form.

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import EventPage from './EventPage';
@@ -8,7 +8,18 @@ import MyEvents from './MyEvents';
 import Footer from './footer';
 import Notifications from './Notifications';
 
+// Import the functions you need from the SDKs                      you need
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref } from "firebase/database";
+import { firebaseConfig } from '../config';
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+const postsRef = ref(db, 'posts');
+
 export default function App(props) {
+  
   return (
     <div>
       <Nav />
@@ -16,7 +27,7 @@ export default function App(props) {
           <Route path="/Home" element={<Home />} />
           <Route path="/CreateEvent" element={<Form />} />
           <Route path="/MyEvent" element={<MyEvents />} />
-          <Route path="/Notifications" element={<Notifications />} />
+          <Route path="/Notifications" element={<Notifications />} ref={postsRef} />
           <Route path="/EventPage" element={<EventPage />} />
           <Route path='/' element={<Home />} />
       </Routes>
